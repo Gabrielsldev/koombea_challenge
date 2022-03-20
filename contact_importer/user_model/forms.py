c2f57ec42c3e4698bcbf7e20fb7db94fa55b39eb
Gabrielsldev <gabrielsl.dev@gmail.com>
2022-03-20 19:46:51 -0300

    Styled login and sign up pages

--- contact_importer/user_model/forms.py
+++ contact_importer/user_model/forms.py
@@ -8,14 +8,23 @@ User = get_user_model()
 
 # Sign Up Form
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(max_length=20, required=True, help_text='Username')
-    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+    email = forms.EmailField(help_text="",label="", required=True, widget=forms.EmailInput(attrs={"class":"form-control", "placeholder":"Enter Email"}))
 
     class Meta:
         model = User
-        fields = [
-            'username', 
-            'email', 
-            'password1', 
-            'password2', 
-            ]
+        fields = ['username', 'email', 'password1', 'password2']
+
+    # For formatting the fields that are built in UserCreationForm
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs["class"] = "form-control"
+        self.fields['username'].widget.attrs["placeholder"] = "Username"
+        self.fields['username'].label = ""
+        self.fields['username'].help_text = ""
+        self.fields['password1'].widget.attrs["class"] = "form-control"
+        self.fields['password1'].widget.attrs["placeholder"] = "Password"
+        self.fields['password1'].label = ""
+        self.fields['password2'].widget.attrs["placeholder"] = "Confirm Password"
+        self.fields['password2'].widget.attrs["class"] = "form-control"
+        self.fields['password2'].label = ""
+        self.fields['password2'].help_text = ""
